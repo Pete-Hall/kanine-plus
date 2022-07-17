a38d26142660e8fe78c14904cc6e41445cc3268b
Pete Hall <pete.mack.hall@gmail.com>
2022-07-17 15:15:00 -0500

    npm installed, changed server to 5001, added user table and roles.

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -5,10 +5,18 @@ import {useSelector} from 'react-redux';
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  
+  let userRole = 'user';
+  if( user.role === 2) {
+    userRole = 'admin'
+  };
+
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
+      <p>Your role # is: {user.role}</p>
+      <p>Your role is: {userRole}</p>
       <LogOutButton className="btn" />
     </div>
   );
